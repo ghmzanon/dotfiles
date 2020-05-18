1e88b05383780811a911095fb0f182de7fdfa4e6
Gustavo H. M. Zanon <ghmzanon@gmail.com>
2020-05-18 20:53:13 -0300

    Add vpn and rdm connection scripts

---
+++ .bin/rdm-scopus-connect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xfreerdp /v:SCPWSOL103335 /d:SCOPUS /u:gzanon /p:password \
+	+auto-reconnect /size:2560x1080 /f -themes -wallpaper /gfx \
+	/sound /microphone
---
+++ .bin/vpn-scopus-connect.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo openconnect --protocol=gp --user=gzanon --quiet --background stc.scopus.com.br
